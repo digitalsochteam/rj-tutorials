ce2f1fa94cb7697ac53c180845e1eea624817777
digitalsochteam <digitalsochgithub@gmail.com>
2026-04-22 15:30:05 +0530

    google review add

---
+++ app/Http/Middleware/RedirectToWww.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class RedirectToWww
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!str_starts_with($request->getHost(), 'www.')) {
+            $url = 'https://www.' . $request->getHost() . $request->getRequestUri();
+            return redirect($url, 301);
+        }
+
+        return $next($request);
+    }
+}
