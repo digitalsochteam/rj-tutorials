ea3aa84ccd154812e754446b4ecf5fdc7abe553f
digitalsochteam <digitalsochgithub@gmail.com>
2026-04-23 15:34:21 +0530

    google review add

--- resources/views/Frontend/404.blade.php
+++ resources/views/Frontend/404.blade.php
@@ -4,19 +4,22 @@
     <div class="container text-center" style="margin-top: 100px;">
         <h1 class="display-4">404 - Page Not Found</h1>
         <p class="lead">Sorry, the page you are looking for does not exist.</p>
-        <a href="/dashboard" class="btn btn-primary mt-3">Go to Dashboard</a>
+        <a href="{{ route('dashboard') }}" class="btn btn-primary mt-3">Go to Dashboard</a>
         <p class="mt-4">You will be redirected to the homepage in <span id="countdown">2</span> seconds.</p>
     </div>
     <script>
         let seconds = 2;
         const countdown = document.getElementById('countdown');
-        setInterval(function () {
+        const timer = setInterval(function () {
             seconds--;
-            if (seconds <= 0) {
-                window.location.href = '/';
-            } else {
+            if (seconds > 0) {
                 countdown.textContent = seconds;
             }
         }, 1000);
+
+        setTimeout(function () {
+            clearInterval(timer);
+            window.location.href = '/';
+        }, 2000);
     </script>
 @endsection
